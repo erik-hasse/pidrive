c8b2a7080a5b9eca412a6265c089cd4cf65abd4a
Erik Hasse <erik.g.hasse@gmail.com>
2020-10-23 03:52:28 +0000

    Fix import name

--- pidrive/hardware/__init__.py
+++ pidrive/hardware/__init__.py
@@ -1,4 +1,4 @@
 from pidrive.hardware.PCA9685 import PCA9685
 from pidrive.hardware.TB6612 import TB6612Motor
 from pidrive.hardware.PWMServo import PWMServo
-from pidrive.hardware.SunFounder_Picar_V import PiCarV
+from pidrive.hardware.SunFounder_PiCarV import PiCarV
